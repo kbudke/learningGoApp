1b280d39849a6897d4ff5ed30dcf2ed8da3230e2
kxb6856 <kyle_budke@homedepot.com>
2019-07-07 00:49:03 -0400

    fixing area switch case

--- learningGoApp/area/area_test.go
+++ learningGoApp/area/area_test.go
@@ -1,1 +1,46 @@
 package area_test
+
+import "fmt"
+
+var _ = Describe("Area", func() {
+
+	var (
+		circleRadius, squareLength float64
+	)
+
+	BeforeEach(func() {
+		fmt.Println("Running test case.")
+		circleRadius = 1
+		squareLength = 4
+	})
+
+	AfterEach(func() {
+		fmt.Println("Test case executed.")
+	})
+
+	Describe("Testing Areas.", func() {
+		Context("calculating Area of Circle with radius of 3.", func() {
+			It("Area of a circle with radius 1 should be 3.14 ", func() {
+				Expect(calculator.Sum(num1, num2)).To(Equal((int32(10))))
+			})
+		})
+
+		Context("calculating subtraction of (6, 4).", func() {
+			It("Subtraction of 6 and 4 should be -2", func() {
+				Expect(calculator.Sub(num1, num2)).To(Equal((int32(-2))))
+			})
+		})
+
+		Context("calculating multiplication of (6, 4).", func() {
+			It("Multiplication of 6 and 4 should be 24", func() {
+				Expect(calculator.Multi(num1, num2)).To(Equal((int32(24))))
+			})
+		})
+
+		Context("calculating square root of (9).", func() {
+			It("Square root of 9 should be 3.", func() {
+				Expect(calculator.Sqrt(9)).To(Equal((float64(3))))
+			})
+		})
+	})
+})
--- learningGoApp/main.go
+++ learningGoApp/main.go
@@ -5,98 +5,82 @@ import (
 	"os"
 	"strconv"
 
-	tryHSTS "./http"
 	a "./area"
 	"./calculator"
 )
 
+var num1 string
+var num2 string
+
+func GetNums() (num1, num2) {
+
+	fmt.Print("Input the first number: ")
+	fmt.Scanln(&num1)
+
+	fmt.Print("Input the second number: ")
+	fmt.Scanln(&num2)
+}
+
 func main() {
-
-	hsts := tryHSTS.serveHTTPS() {
-
-	}
-	
-	func hsts() {
-	if *serveHTTPSFlag {
-		serveHTTPS()
-	} else {
-		serveHTTP()
-	}
-	}
-	//TODO: finish area calculations
-
-	// circ := a.Circle{
-	// 	radius: 12.345,
-	// }
-	// sqr := a.Square{
-	// 	length: 15,
-	// }
-	// info(circ)
-	// info(sqr)
-
-
-	// TODO: set up the first switch for is this a regular math opperation
-	// TODO: or is this to calculate area?
-
-	hours, remainder := Divmod(5566, 3600)
-	minutes, seconds := Divmod(remainder, 60)
-	fmt.Printf("%d:%d:%d", hours, minutes, seconds)
 
 	//getOperation is a label, use labels to define sections within a single func or multi func
 	//we use as a checkpoint under main, to choose an operation.
 getOperation:
-	var operation string
-	fmt.Print("Select an operation: +, -, *, / : ")
-	//Scanln is similar to Scan, but stops scanning at a newline and after the final item there must be a newline or EOF.
-	fmt.Scanln(&operation)
 
-	if area {
-		fmt.Print("Area can help with Calculating the area of the following shapes.")
-		fmt.Println(area.Square)
-	}
+	var mathOperation string
+	var calculatorOperation string
+	switch typeOfOperation {
+	case mathOperation:
+		switch mathOperation {
+		case "+":
+			var firstNumber int = stringToInt(num1)
+			var secondNumber int = stringToInt(num2)
+			fmt.Print("Result: ")
+			fmt.Println(calculator.Add(firstNumber, secondNumber))
+		case "-":
+			var firstNumber int = stringToInt(num1)
+			var secondNumber int = stringToInt(num2)
+			fmt.Print("Result: ")
+			fmt.Println(calculator.Subtract(firstNumber, secondNumber))
+		case "*":
+			var firstNumber float64 = stringToFloat64(num1)
+			var secondNumber float64 = stringToFloat64(num2)
+			fmt.Print("Result: ")
+			fmt.Println(calculator.Multiply(firstNumber, secondNumber))
+		case "/":
+			var firstNumber float64 = stringToFloat64(num1)
+			var secondNumber float64 = stringToFloat64(num2)
+			fmt.Print("Result: ")
+			fmt.Println(calculator.Divide(firstNumber, secondNumber))
 
-	var num1 string
-	fmt.Print("Input the first number: ")
-	fmt.Scanln(&num1)
+			// fmt.Print("Select an operation: +, -, *, / : ")
+			// //Scanln is similar to Scan, but stops scanning at a newline and after the final item there must be a newline or EOF.
+			// fmt.Scanln(&operation)
+			// GetNums()
+		}
+	case areaOperation:
+		switch areaOperation {
+		case "circle":
+			var radius float64
+			var area float64 = stringToFloat64(Circle)
+			fmt.Print("Result: ")
+			fmt.Println(a.Circle(radius))
+		case "square":
+			var area float64 = stringToFloat64(Square)
+			var length float64
 
-	var num2 string
-	fmt.Print("Input the second number: ")
-	fmt.Scanln(&num2)
+			fmt.Print("Result: ")
+			fmt.Println(a.Square(length))
 
-	switch operation {
-	case "+":
-		var firstNumber int = stringToInt(num1)
-		var secondNumber int = stringToInt(num2)
-		fmt.Print("Result: ")
-		fmt.Println(calculator.Add(firstNumber, secondNumber))
-	case "-":
-		var firstNumber int = stringToInt(num1)
-		var secondNumber int = stringToInt(num2)
-		fmt.Print("Result: ")
-		fmt.Println(calculator.Subtract(firstNumber, secondNumber))
-
-	case "*":
-		var firstNumber float64 = stringToFloat64(num1)
-		var secondNumber float64 = stringToFloat64(num2)
-		fmt.Print("Result: ")
-		fmt.Println(calculator.Multiply(firstNumber, secondNumber))
-
-	case "/":
-		var firstNumber float64 = stringToFloat64(num1)
-		var secondNumber float64 = stringToFloat64(num2)
-		fmt.Print("Result: ")
-		fmt.Println(calculator.Divide(firstNumber, secondNumber))
-
-	case "circle":
-		var radius float64
-		fmt.Print("Result: ")
-		fmt.Println(a.Circle(radius))
-
-	case "square":
-		var length float64
-
-		fmt.Print("Result: ")
-		fmt.Println(a.Square(length))
+		}
+		fmt.Print("Select the shape you want area Calculated: Circle, Square : ")
+		fmt.Scanln(&calculatorOperation)
+		var circle float64
+		fmt.Print("Input the Radius of the circle: ")
+		fmt.Scanln(&radius)
+		var square float64
+		fmt.Print("Input a Length of the square: ")
+		smt.Scanln(&length)
 
 	default:
 		fmt.Println("Invalid operation selected. Please try again!")
@@ -104,6 +88,30 @@ getOperation:
 		goto getOperation
 	}
 }
+
+// TODO test security headers
+// hsts := tryHSTS.serveHTTPS() {
+
+// }
+
+// func hsts() {
+// if *serveHTTPSFlag {
+// 	serveHTTPS()
+// } else {
+// 	serveHTTP()
+// }
+// }
+
+//TODO: finish area calculations
+
+// circ := a.Circle{
+// 	radius: 12.345,
+// }
+// sqr := a.Square{
+// 	length: 15,
+// }
+// info(circ)
+// info(sqr)
 
 func stringToInt(str string) int {
 	i, err := strconv.Atoi(str)
