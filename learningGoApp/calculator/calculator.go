ef63e7d5c2e2d28c67ed475e1636a0adbcc6494e
kxb6856 <kyle_budke@homedepot.com>
2019-07-12 13:38:38 -0400

    updated calculator, removed text explanation, need to remove switch in lue of regEx

--- learningGoApp/calculator/calculator.go
+++ learningGoApp/calculator/calculator.go
@@ -5,6 +5,7 @@ import (
 	"math"
 	"os"
 	"strconv"
+	"strings"
 )
 
 func Add(num1, num2 int) int {
@@ -56,51 +57,94 @@ func stringToFloat64(str string) float64 {
 	return f
 }
 
+func float64ToString(float float64) string {
+	return fmt.Sprintf("%f", float)
+}
+func calculateExpression(expr []string) float64 {
+	var solution float64
+	// remainingExpr := append(expr)
+evaluateMore:
+	for i := 0; i < len(expr); i++ {
+		switch expr[i] {
+		case "+":
+			var calculation = stringToFloat64(expr[i-1]) + stringToFloat64(expr[i+1])
+			ans := []string{float64ToString(calculation)}
+			expr = append(ans, expr[i+2:]...)
+			goto evaluateMore
+		case "-":
+
+		case "*":
+
+		case "=":
+			solution = stringToFloat64(expr[i-1])
+		}
+
+	}
+	return solution
+}
+
 //ArithmeticCalculator main controller
 func ArithmeticCalculator(arithmetic string) {
-
+	executionArray := []string{}
 getOperation:
 	// var arithmetic string
 	// fmt.Print("Please select an operation: +, -, *, / : ")
 	// fmt.Scanln(&arithmetic)
-	var num1 string
-	fmt.Print("Input the first number: ")
-	fmt.Scanln(&num1)
 
-	var num2 string
-	fmt.Print("Input the second number: ")
-	fmt.Scanln(&num2)
+	for {
+		// TODO: can we note the thought process of anything random set up and or why we went with it..
+		var input string
+		fmt.Scanln(&input)
+		if input != "=" {
+			executionArray = append(executionArray, input)
+		} else {
+			executionArray = append(executionArray, input)
+			solution := calculateExpression(executionArray)
+			fmt.Println(strings.Join(executionArray, " "), solution)
+		}
+		// fmt.Println(executionArray)
+		// var num1 string
+		// fmt.Print("Input the first number: ")
+		// fmt.Scanln(&num1)
 
-	switch arithmetic {
-	case "+":
-		var firstNumber = stringToInt(num1)
-		var secondNumber = stringToInt(num2)
-		fmt.Print("Result: ")
-		fmt.Println(Add(firstNumber, secondNumber))
-	case "-":
-		var firstNumber = stringToInt(num1)
-		var secondNumber = stringToInt(num2)
-		fmt.Print("Result: ")
-		fmt.Println(Subtract(firstNumber, secondNumber))
-	case "*":
-		var firstNumber = stringToFloat64(num1)
-		var secondNumber = stringToFloat64(num2)
-		fmt.Print("Result: ")
-		fmt.Println(Multiply(firstNumber, secondNumber))
-	case "/":
-		var firstNumber = stringToFloat64(num1)
-		var secondNumber = stringToFloat64(num2)
-		fmt.Print("Result: ")
-		fmt.Println(Divide(firstNumber, secondNumber))
-	// case "back":
-	// 	return
-	default:
-		fmt.Println("Invalid operation selected. Please try again!")
-		//The goto in this case saves us from introducing another (boolean) variable used just for control-flow, checked for at the end. In this case, the goto statement makes the code actually better to read and easier follow
+		// var num2 string
+		// fmt.Print("Input the second number: ")
+		// fmt.Scanln(&num2)
+
+		// switch input {
+		// case "+":
+		// 	var firstNumber = stringToInt(num1)
+		// 	var secondNumber = stringToInt(num2)
+		// 	fmt.Print("Result: ")
+		// 	fmt.Println(Add(firstNumber, secondNumber))
+		// case "-":
+		// 	var firstNumber = stringToInt(num1)
+		// 	var secondNumber = stringToInt(num2)
+		// 	fmt.Print("Result: ")
+		// 	fmt.Println(Subtract(firstNumber, secondNumber))
+		// case "*":
+		// 	var firstNumber = stringToFloat64(num1)
+		// 	var secondNumber = stringToFloat64(num2)
+		// 	fmt.Print("Result: ")
+		// 	fmt.Println(Multiply(firstNumber, secondNumber))
+		// case "/":
+		// 	var firstNumber = stringToFloat64(num1)
+		// 	var secondNumber = stringToFloat64(num2)
+		// 	fmt.Print("Result: ")
+		// 	fmt.Println(Divide(firstNumber, secondNumber))
+		// // case "back":
+		// // 	return
+		// case "=":
+
+		// default:
+		// 	fmt.Println("Invalid operation selected. Please try again!")
+		// 	//The goto in this case saves us from introducing another (boolean) variable used just for control-flow, checked for at the end. In this case, the goto statement makes the code actually better to read and easier follow
+		// 	goto getOperation
+		// }
+
+		// fmt.Print("Please select an operation: +, -, *, / : ")
+		// fmt.Scanln(&arithmetic)
 		goto getOperation
 	}
 
-	fmt.Print("Please select an operation: +, -, *, / : ")
-	fmt.Scanln(&arithmetic)
-	goto getOperation
 }
